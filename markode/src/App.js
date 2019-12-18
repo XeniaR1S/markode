26212f40f69311c1b06ba1b01683e6db658c7fd2
Jessy Pinchon <jessy.pinchon@gmail.com>
2019-12-18 19:33:32 +0100

    navbar created + search field

--- markode/src/App.js
+++ markode/src/App.js
@@ -3,15 +3,30 @@ import Main from './components/Main';
 import './App.css';
 import Navbar from './components/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="navbar">
-        <Navbar />
-      </header>
-      <Main/>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      input:'',
+    }
+    this.searchField=this.searchField.bind(this)
+  }
+
+  searchField = (event)=>{
+    this.setState({input: event.target.value})
+    console.log(event.target.value)
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="navbar">
+          <Navbar searchField={this.searchField} input={this.state.input}/>
+        </header>
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
--- markode/src/components/Navbar.jsx
+++ markode/src/components/Navbar.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import '../App.css';
 
 
-function Navbar() {
+//button onClick = {props.getWords}
+function Navbar(props) {
   return (
-    <div className="App">
-      <h1>marKode</h1>
-      <input onChange={props.searchField} value={props.input} id='searchField' type = 'text' placeholder = 'mot'></input>
-      <button onClick = {props.getWords} className='navButton' value ='search'>Rechercher</button>
+    <div className = 'navbar_container'>
+      <div>
+        <h1><img src='logo_markode.png' alt='logo' className='navbar_title'/></h1>
+      </div>
+      <div className='search_container'>
+        <input onChange={props.searchField} value={props.input} id='searchField' type ='text' placeholder ='Rechercher un mot ou chaîne'></input>
+        <button value ='search'>
+          <img src='icon_search.svg' className='navbar_search_btn' alt='button_search' />
+        </button>
+      </div>
+      <div className='search_method'>
+        <label for='word'>
+          <input type='radio' id='word' name='method' value='word' />
+          Rechercher par mot
+        </label>
+        <label for="string">
+          <input type='radio' id='string' name='method' value='string' />
+          Rechercher par chaîne
+        </label>
+      </div>
     </div>
   );
 }
