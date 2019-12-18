5553b59727b7a18bf8d984560fd8286b4dd9d60a
Pascal <fedodev55@gmail.com>
2019-12-18 16:03:05 +0100

    initialyse create nav tool

---
+++ markode/src/components/navTools/NavTools.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './navTools.css';
+
+export default class NavTools extends Component {
+  render() {
+    return (
+      <div className="navTool">
+        <h3>Outils markdown</h3>
+      </div>
+    );
+  }
+}
