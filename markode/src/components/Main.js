e9a29606f1667aacd9ee5008269c402ee2766037
Pierre Moulin <phv.moulin@gmail.com>
2019-12-18 16:31:04 +0100

    create textarea and update box model

--- markode/src/components/Main.js
+++ markode/src/components/Main.js
@@ -4,6 +4,14 @@ function Main() {
     return (
         <div className="main">
             <div className="textBlocs">
+                <div className="mdBox">
+                    <textarea className="textEditors"/>
+                    <button>Effacer tout</button>                    
+                </div>
+                <div className="htmlBox">
+                    <textarea className="textEditors"/>
+                    <button>Exporter</button>    
+                </div>
             </div>
         </div>
     )
