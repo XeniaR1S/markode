bb39706e36d0a4c713e5a36fd5c725dc355bfe3f
Jessy Pinchon <jessy.pinchon@gmail.com>
2019-12-18 16:05:29 +0100

    before flex container

--- markode/src/components/Navbar.jsx
+++ markode/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+
 
 function Navbar() {
   return (
     <div className="App">
-
+      <h1>marKode</h1>
+      <input onChange={props.searchField} value={props.input} id='searchField' type = 'text' placeholder = 'mot'></input>
+      <button onClick = {props.getWords} className='navButton' value ='search'>Rechercher</button>
     </div>
   );
 }
