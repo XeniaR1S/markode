0b59be17885c9cee86ca99afcf190506035827d0
Pierre Moulin <phv.moulin@gmail.com>
2019-12-19 09:46:44 +0100

    main.js : typing in textarea updates state

--- markode/src/components/Main.js
+++ markode/src/components/Main.js
@@ -1,20 +1,57 @@
 import React from 'react';
 
-function Main() {
-    return (
-        <div className="main">
-            <div className="textBlocs">
-                <div className="mdBox">
-                    <textarea className="textEditors"/>
-                    <button className="editorButtons">Effacer tout</button>                    
-                </div>
-                <div className="htmlBox">
-                    <textarea className="textEditors"/>
-                    <button className="editorButtons">Exporter</button>    
+const MarkdownIt = require('markdown-it')
+const md = new MarkdownIt();
+let mdText = 
+  (`- bla
+  - blabla
+  - blablabla`
+  )
+let result = md.render(mdText);
+
+class Main extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            text: ""
+        }
+        this.mdToHtml = this.mdToHtml.bind(this)
+        this.onChange = this.onChange.bind(this)
+    }
+    onChange(e){
+        this.setState({
+            text: e.target.value
+        });
+    }
+    mdToHtml() {
+        const MarkdownIt = require('markdown-it')
+        const md = new MarkdownIt();
+        let mdText = 
+        (`- bla
+        - blabla
+        - blablabla`
+        )
+        let result = md.render(mdText);
+    }
+    render() {
+        return (
+            <div className="main">
+                <div className="textBlocs">
+                    <div className="mdBox">
+                        <textarea 
+                            className="textEditors"
+                            onChange={this.onChange}>
+                        </textarea>
+                        <button className="editorButtons">Effacer tout</button>                    
+                    </div>
+                    <div className="htmlBox">
+                        <textarea className="textEditors">{result}</textarea>
+                        <button className="editorButtons">Exporter</button>    
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Main;
