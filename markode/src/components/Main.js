b0acb6eb2361d5be323f60725caf264e264c2303
Jessy Pinchon <jessy.pinchon@gmail.com>
2019-12-19 16:46:51 +0100

    navbar called in main

--- markode/src/components/Main.js
+++ markode/src/components/Main.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Footer from './Footer';
 import NavTools from './navTools/NavTools';
+import Navbar from './Navbar'
 
 class Main extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
             text: "",
-            numChar: 0
+            numChar: 0,
+            input: ''
         }
         this.mdToHtml = this.mdToHtml.bind(this);
         this.countChar = this.countChar.bind(this);
@@ -29,27 +31,47 @@ class Main extends React.Component {
         this.mdToHtml(e)
         this.countChar(e)
     }
+
+    searchField = (event)=>{
+        this.setState({input: event.target.value})
+    }
+
+    // getWord = () =>{
+    //     this.state.text.includes(event.target.value)
+    //     if (this.state.text === event.target.value){
+    //         console.log(this.state.text)
+    //     }
+    // }
+
+
+
+
     render() {
         return (
-            <div className="main">
-                <NavTools />
-                <div className="textBlocs">
-                    <div className="mdBox">
-                        <textarea 
-                            className="textEditors"
-                            onChange={this.combinedMethods}>
-                        </textarea>
-                        <button className="editorButtons">Effacer tout</button>                    
+            <div>
+                <header className="navbar">
+                    <Navbar searchField={this.searchField} input={this.state.input} />
+                </header>
+                <div className="main">
+                    <NavTools />
+                    <div className="textBlocs">
+                        <div className="mdBox">
+                            <textarea 
+                                className="textEditors"
+                                onChange={this.combinedMethods}>
+                            </textarea>
+                            <button className="editorButtons">Effacer tout</button>                    
+                        </div>
+                        <div className="htmlBox">
+                            <textarea 
+                                readOnly
+                                className="textEditors"
+                                value={this.state.text}></textarea>
+                            <button className="editorButtons">Exporter</button>    
+                        </div>
                     </div>
-                    <div className="htmlBox">
-                        <textarea 
-                            readOnly
-                            className="textEditors"
-                            value={this.state.text}></textarea>
-                        <button className="editorButtons">Exporter</button>    
-                    </div>
+                    <Footer countChar={this.countChar} numChar={this.state.numChar}/>
                 </div>
-                <Footer countChar={this.countChar} numChar={this.state.numChar}/>
             </div>
         )
     }
