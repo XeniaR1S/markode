9f2c76375dd9053b9ed74238162f91072a16e933
Pascal <fedodev55@gmail.com>
2019-12-19 10:49:01 +0100

    change techno for nav Tool

--- markode/src/components/navTools/NavTools.js
+++ markode/src/components/navTools/NavTools.js
@@ -1,43 +1,45 @@
 import React, { Component } from 'react';
-import './navTools.css';
+import '../../App.css';
 import Collapsible from 'react-collapsible';
 
 export default class NavTools extends Component {
   render() {
     return (
       <div className="navTool">
-        <h3>Outils markdown</h3>
-        <Collapsible trigger="Mise en forme" className="titleTool">
-          <p>Gras</p>
-          <p>Italique</p>
-        </Collapsible>
-        <Collapsible trigger="Titres" className="titleTool">
-          <p>&lt; H1 &gt;</p>
-          <p>&lt; H2 &gt;</p>
-          <p>&lt; H3 &gt;</p>
-          <p>&lt; H4 &gt;</p>
-          <p>&lt; H5 &gt;</p>
-          <p>&lt; H6 &gt;</p>
-        </Collapsible>
-        <Collapsible trigger="Listes" className="titleTool">
-          <p>Listes à puces</p>
-          <p>Listes numérotées</p>
-          <p>Listes imbriquées</p>
-        </Collapsible>
-        <Collapsible trigger="Blocs" className="titleTool">
-          <p>Blocs de citation</p>
-          <p>Code incorporé</p>
-          <p>Blocs de code</p>
-        </Collapsible>
-        <Collapsible trigger="Liens" className="titleTool">
-          <p>Liens vers les tickets</p>
-          <p>Images servant de liens</p>
-        </Collapsible>
-        <Collapsible trigger="Images" className="titleTool"></Collapsible>
-        <Collapsible trigger="Mise en page" className="titleTool">
-          <p>Ligne de séparation horizontale</p>
-          <p>Italique</p>
-        </Collapsible>
+        <h3 className="titleTool">Outils markdown</h3>
+        <div className="accordion">
+          <section id="item1">
+            <a href="#item1">
+              <h3>Texte</h3>
+            </a>
+            <h5>Gras</h5>
+            <h5>Italique</h5>
+            <h5>Souligné</h5>
+            <h5>Barré</h5>
+          </section>
+          <section id="item2">
+            <a href="#item2">
+              <h3>Listes</h3>
+            </a>
+            <h5>Listes à puces</h5>
+            <h5>Listes numérotées</h5>
+            <h5>Listes imbriquées</h5>
+          </section>
+          <section id="item3">
+            <a href="#item3">
+              <h3>Mise en forme</h3>
+            </a>
+            <h5>Ligne de séparation horizontale</h5>
+          </section>
+          <section id="item4">
+            <a href="#item4">
+              <h3>Blocs</h3>
+            </a>
+            <h5>Blocs de citation</h5>
+            <h5>Code incorporé</h5>
+            <h5>Blocs de code</h5>
+          </section>
+        </div>
       </div>
     );
   }
